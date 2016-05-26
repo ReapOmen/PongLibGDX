09c2b7d84746bd4c7fa1b6d0bbc1f0d53d1ea431
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-05-26 22:05:12 +0100

    removed some repetition and added paddles

--- Pong-core/src/com/robert/pong/GameWorld.java
+++ Pong-core/src/com/robert/pong/GameWorld.java
@@ -7,59 +7,38 @@ import com.badlogic.gdx.math.Vector2;
 
 public class GameWorld {
 	
-	private Rectangle firstPong, secondPong;
-	private float gameWidth, gameHeight;
+	private Paddle firstPaddle, secondPaddle;
+	private int gameWidth, gameHeight;
 	private Circle ball;
 	private boolean started;
 	private Vector2 ballOffset;
 	
-	public GameWorld(float width, float height) {
+	public GameWorld(int width, int height) {
 		
 		gameWidth = width;
 		gameHeight = height;
-		firstPong = new Rectangle(10, height/2 - 15, 15, 80);
-		secondPong = new Rectangle(width - 25, height/2 - 15, 15, 80);
+		
+		firstPaddle = new Paddle(gameWidth, gameHeight, Paddle.ONE);
+		secondPaddle = new Paddle(gameWidth, gameHeight, Paddle.TWO);
+		
 		ball = new Circle(gameWidth/2, gameHeight/2, 7);
+		
 		started = false;
 	}
 	
-	public Rectangle getFirstPong() {
+	public Paddle getFirstPaddle() {
 		
-		return firstPong;
+		return firstPaddle;
 	}
 	
-	public Rectangle getSecondPong() {
+	public Paddle getSecondPaddle() {
 		
-		return secondPong;
+		return secondPaddle;
 	}
 	
 	public Circle getBall() {
 		
 		return ball;
-	}
-	
-	public void moveFirstUp() {
-		
-		if(firstPong.y + firstPong.height < gameHeight)
-			firstPong.y += 10;
-	}
-	
-	public void moveFirstDown() {
-		
-		if(firstPong.y > 0)
-			firstPong.y -= 10;
-	}
-	
-	public void moveSecondUp() {
-		
-		if(secondPong.y + secondPong.height < gameHeight)
-			secondPong.y += 10;
-	}
-	
-	public void moveSecondDown() {
-		
-		if(secondPong.y > 0)
-			secondPong.y -= 10;
 	}
 	
 	public void updateBall() {
@@ -68,8 +47,8 @@ public class GameWorld {
 			
 			started = true;
 			ballOffset = new Vector2();
-			ballOffset.x = 2.0f;
-			ballOffset.y = 2.0f;
+			ballOffset.x = 2.5f;
+			ballOffset.y = 2.5f;
 		}
 		else {
 			
@@ -78,11 +57,11 @@ public class GameWorld {
 			float x = ball.x;
 			float y = ball.y;
 			
-			if(x >= gameWidth - ball.radius)
-				ballOffset.x = -ballOffset.x;
-			else
-				if(x <= ball.radius)
-					ballOffset.x = -ballOffset.x;
+			if(x >= gameWidth - ball.radius || x <= ball.radius) {
+				started = false;
+				ball.setPosition(gameWidth/2, gameHeight/2);
+			}
+
 			
 			if(y >= gameHeight - ball.radius)
 				ballOffset.y = -ballOffset.y;
@@ -90,14 +69,42 @@ public class GameWorld {
 				if(y <= ball.radius)
 					ballOffset.y = -ballOffset.y;
 			
-			if(Intersector.overlaps(ball, firstPong)) {
+			if(Intersector.overlaps(ball, firstPaddle.getRectangle())) {
 				
 				if(ballOffset.y > 0.0)
 					ballOffset.rotate(-90);
 				else
 					ballOffset.rotate(90);
 			}
+			
+			if(Intersector.overlaps(ball, secondPaddle.getRectangle())) {
+	
+				if(ballOffset.y > 0.0)
+					ballOffset.rotate(90);
+				else
+					ballOffset.rotate(-90);
+			}
 		}
+	}
+	
+	public void moveFirstUp() {
+		
+		firstPaddle.moveUp();
+	}
+	
+	public void moveSecondUp() {
+		
+		secondPaddle.moveUp();
+	}
+	
+	public void moveFirstDown() {
+		
+		firstPaddle.moveDown();
+	}
+	
+	public void moveSecondDown() {
+		
+		secondPaddle.moveDown();
 	}
 	
 }
