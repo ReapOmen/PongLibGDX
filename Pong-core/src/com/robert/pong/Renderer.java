e8fff7fcea20e5b1791a2771dbf683269642f144
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-05-29 23:39:53 +0100

    added javadoc

--- Pong-core/src/com/robert/pong/Renderer.java
+++ Pong-core/src/com/robert/pong/Renderer.java
@@ -3,17 +3,29 @@ package com.robert.pong;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 
+/**
+ * Represents the visual side of the world and takes care of the rendering of the GameWorld.
+ * @author robert
+ *
+ */
 public class Renderer {
 	
 	private ShapeRenderer2 sr;
 	private GameWorld world;
 	
+	/**
+	 * Creates a renderer which will render everything from the GameWorld provided.
+	 * @param w the GameWorld which will be renderer.
+	 */
 	public Renderer(GameWorld w) {
 		
 		world = w;
 		sr = new ShapeRenderer2();
 	}
 	
+	/**
+	 * Renders/draws everything that exists in the GameWorld provided in the constructor.
+	 */
 	public void render() {
 		
 		handleInput();
@@ -24,7 +36,10 @@ public class Renderer {
 		sr.drawBall(world.getBall());
 	
 	}
-
+	
+	/**
+	 * Handles different types of input.
+	 */
 	private void handleInput() {
 		
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
