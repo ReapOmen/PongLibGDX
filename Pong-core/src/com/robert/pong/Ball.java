de305415bbc508bda05192dee254dcb292910018
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-05-29 23:30:13 +0100

    added javadoc + removed unused methods

--- Pong-core/src/com/robert/pong/Ball.java
+++ Pong-core/src/com/robert/pong/Ball.java
@@ -6,13 +6,25 @@ import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Represents the ball in the game of Pong.
+ * @author robert
+ *
+ */
 public class Ball {
 	
 	private Circle circ;
-	private int screenWidth, screenHeight;
-	private Vector2 position, speed;
-	private Vector2 initialState;
 	
+	private int screenWidth, screenHeight;
+	
+	private Vector2 position, speed;
+	private Vector2 initialState; // the initial state of the ball
+	
+	/**
+	 * Creates a ball that can be moved at a certain speed.
+	 * @param gameWidth the width of the screen
+	 * @param gameHeight the height of the screen
+	 */
 	public Ball(int gameWidth, int gameHeight) {
 		
 		screenWidth = gameWidth;
@@ -25,15 +37,21 @@ public class Ball {
 		initialState = new Vector2(x, y);
 		speed = new Vector2(3.5f, 3.5f);
 		
-		
 		circ = new Circle(x, y, 7);
 	}
 	
+	/**
+	 * Gets the circle that represents this ball.
+	 * @return the circle representing this ball.
+	 */
 	public Circle getCircle() {
 		
 		return circ;
 	}
 	
+	/**
+	 * Rotates the speed vector by a random amount.
+	 */
 	public void randomiseRotation() {
 		
 
@@ -42,40 +60,55 @@ public class Ball {
 		speed.rotate(x);
 	}
 	
+	/**
+	 * Updates the position of the ball using the speed vector.
+	 */
 	public void update() {
 		
 		position.add(speed);
 		circ.setPosition(position);
 	}
 	
+	/**
+	 * Sets the rotation of the speed vector.
+	 * @param degrees the amount of degrees the vector should be rotated
+	 */
 	public void setRotation(float degrees) {
 		
 		speed.rotate(degrees);
-		System.out.println("Rotated by: " + degrees);
 	}
 	
-	public void setPosition(Vector2 f) {
-		
-		position.x = f.x;
-		position.y = f.y;
-		circ.setPosition(position);
-	}
-	
+	/**
+	 * Gets the position vector of this ball (the coordinates of the center).
+	 * @return the ball's current position.
+	 */
 	public Vector2 getPosition() {
 		
 		return position;
 	}
 	
+	/**
+	 * Gets the initial state vector (position) of this ball.
+	 * @return the initial state vector of this ball.
+	 */
 	public Vector2 getInitialState() {
 		
 		return initialState;
 	}
 	
+	/**
+	 * Gets the speed vector of this ball.
+	 * @return the speed vector of this ball.
+	 */
 	public Vector2 getSpeed() {
 		
 		return speed;
 	}
 	
+	/**
+	 * Gets the radius of this ball.
+	 * @return the radius of this ball.
+	 */
 	public float getRadius() {
 		
 		return circ.radius;
