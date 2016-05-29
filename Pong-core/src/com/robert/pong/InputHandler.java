418d54cce24379a4a1ebec1b3fdbe46d1cd1110a
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-05-29 23:36:50 +0100

    added javadoc

--- Pong-core/src/com/robert/pong/InputHandler.java
+++ Pong-core/src/com/robert/pong/InputHandler.java
@@ -2,10 +2,19 @@ package com.robert.pong;
 
 import com.badlogic.gdx.InputAdapter;
 
+/**
+ * Represents a handler that takes care of certain types of input.
+ * @author robert
+ *
+ */
 public class InputHandler extends InputAdapter {
 	
 	private GameWorld world;
 	
+	/**
+	 * Creates a new InputHandler that will work with the GameWorld provided.
+	 * @param world a GameWorld to which this InputHandler will refer to.
+	 */
 	public InputHandler(GameWorld world) {
 		
 		this.world = world;
