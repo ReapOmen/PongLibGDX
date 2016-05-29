45ceb26b18e784819f3bdd89c2bafc9e102bc3cc
Robert Bartlensky <bartlensky.robert@gmail.com>
2016-05-29 23:07:39 +0100

    removed unnecessary imports

--- Pong-core/src/com/robert/pong/Game.java
+++ Pong-core/src/com/robert/pong/Game.java
@@ -2,11 +2,7 @@ package com.robert.pong;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.Circle;
-import com.badlogic.gdx.math.Rectangle;
 
 public class Game extends ApplicationAdapter {
 	
@@ -23,6 +19,7 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
